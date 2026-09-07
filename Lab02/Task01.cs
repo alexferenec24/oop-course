f91c612f0d2fcf59fd322075b1571153cce3db0d
Sasha <alexferenec24@gmail.com>
2026-09-07 21:17:04 +0300

    Lab02 Task01

---
+++ Lab02/Task01.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Lab02
+{
+    public class Task1
+    {
+        public void Run()
+        {
+            Console.Write("Введіть кількість пацієнтів: ");
+            int N = int.Parse(Console.ReadLine()!);
+
+            double[] weights = new double[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write($"Введіть вагу пацієнта {i + 1}: ");
+                weights[i] = double.Parse(Console.ReadLine()!);
+            }
+
+            double sum = 0;
+            double min = weights[0];
+            double max = weights[0];
+
+            foreach (double weight in weights)
+            {
+                sum += weight;
+                if (weight < min) min = weight;
+                if (weight > max) max = weight;
+            }
+
+            double average = sum / N;
+
+            int aboveAverage = 0;
+            foreach (double weight in weights)
+            {
+                if (weight > average) aboveAverage++;
+            }
+
+            Console.WriteLine($"Кількість: {N}");
+            Console.WriteLine($"Середня вага: {average:F1} кг");
+            Console.WriteLine($"Мін / Макс: {min:F1} / {max:F1} кг");
+            Console.WriteLine($"Вище середнього: {aboveAverage}");
+        }
+    }
+}
---
+++ Lab02/Task02.cs
@@ -0,0 +1,6 @@
+﻿namespace Lab02;
+
+public class Task02
+{
+    
+}
